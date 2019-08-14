acfac642a996149555db44096ca0b0565cdb7dfd
George Harutyunyan <>
2019-08-14 17:25:15 -0400

    part 1.12 complete

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,56 +2,34 @@
 import ReactDOM from 'react-dom'
 
 
-const Statistic = ({text,value}) => {
-
-    return (
-        <tr>
-            <td>{text} {value}</td>
-        </tr>
-    )
-}
-
-const Button = ({onClick,text}) => (
+const Button = ({onClick,text }) => (
     <button onClick={onClick}>{text}</button>
 )
 
-const App = props => {
-  const [good,setGood] = useState(0)
-  const [neutral,setNeutral] = useState(0)
-  const [bad,setBad] = useState(0)
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    console.log(props.anecdotes[Math.floor(Math.random()*props.anecdotes.length)])
 
-  if (good+bad+neutral === 0){
-      return (
-          <div>
-            <h2>give feedback</h2>
-            <Button onClick={() => setGood(good+1)} text='good'/>
-            <Button onClick={() => setNeutral(neutral+1)} text='neutral'/>
-            <Button onClick={() => setBad(bad+1)} text='bad'/>
-            <h2>statistics</h2>
-            <p>No feedback given</p>
-          </div>
-
-      )
-  }
-  return (
-      <div>
-        <h2>give feedback</h2>
-        <Button onClick={() => setGood(good+1)} text='good'/>
-        <Button onClick={() => setNeutral(neutral+1)} text='neutral'/>
-        <Button onClick={() => setBad(bad+1)} text='bad'/>
-        <h2>statistics</h2>
-          <table>
-              <tbody>
-                <Statistic text={'good:'} value={good}/>
-                <Statistic text={'neutral:'} value={neutral}/>
-                <Statistic text={'bad:'} value={bad}/>
-                <Statistic text={'all:'} value={good+bad+neutral}/>
-                <Statistic text={'average:'} value={(good-bad)/(good+bad+neutral)}/>
-                <Statistic text={'positive:'} value={(good / (neutral + bad + good)) * 100 + '%'} />
-            </tbody>
-        </table>
-      </div>
-  )
+    return (
+        <div>
+            {props.anecdotes[selected]}
+            <p>
+                <Button onClick={() => setSelected(Math.floor(Math.random() * props.anecdotes.length))} text='next anectode' />
+            </p>
+        </div>
+    )
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
