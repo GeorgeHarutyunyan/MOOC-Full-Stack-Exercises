2f6f111e1604274c98da77315d9ade1c3a79f1cc
Gevorg Harutyunyan - harutyu6 <george.harutyunyan@mail.utoronto.ca>
2019-08-15 02:07:48 -0400

    part 1.13 complete

--- src/index.js
+++ src/index.js
@@ -1,53 +1,55 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Display = ({value}) => (
+    <div>
+        <p>{value}</p>
+    </div>
+)
 
-const Statistic = ({text,value}) => {
+const Button = ({onClick,text }) => (
+    <button onClick={onClick}>{text}</button>
+)
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [votes,setVotes] = useState(new Array(props.anecdotes.length).fill(0))
+    console.log(votes)
+
+    const handleVotes = () => {
+        const copy = [...votes]
+        copy[selected]+=1
+        setVotes(copy)
+    }
+
+    const handleSelect = () => {
+        setSelected(Math.floor(Math.random() * props.anecdotes.length))
+    }
 
     return (
         <div>
-            <p>{text} {value}</p>
+            <Display value={props.anecdotes[selected]}/>
+            <Display value={'has '+votes[selected]+' votes'}/>
+            <p>
+                <Button onClick={() => handleSelect()} text='next anectode' />
+                <Button onClick={() => handleVotes()} text='vote'/>
+            </p>
         </div>
     )
 }
 
-const Button = ({onClick,text}) => (
-    <button onClick={onClick}>{text}</button>
+
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
 )
-
-const App = props => {
-  const [good,setGood] = useState(0)
-  const [neutral,setNeutral] = useState(0)
-  const [bad,setBad] = useState(0)
-
-  if (good+bad+neutral === 0){
-      return (
-          <div>
-            <h2>give feedback</h2>
-            <Button onClick={() => setGood(good+1)} text='good'/>
-            <Button onClick={() => setNeutral(neutral+1)} text='neutral'/>
-            <Button onClick={() => setBad(bad+1)} text='bad'/>
-            <h2>statistics</h2>
-            <p>No feedback given</p>
-          </div>
-
-      )
-  }
-  return (
-      <div>
-        <h2>give feedback</h2>
-        <Button onClick={() => setGood(good+1)} text='good'/>
-        <Button onClick={() => setNeutral(neutral+1)} text='neutral'/>
-        <Button onClick={() => setBad(bad+1)} text='bad'/>
-        <h2>statistics</h2>
-        <Statistic text={'good:'} value={good}/>
-        <Statistic text={'neutral:'} value={neutral}/>
-        <Statistic text={'bad:'} value={bad}/>
-        <Statistic text={'all:'} value={good+bad+neutral}/>
-        <Statistic text={'average:'} value={(good-bad)/(good+bad+neutral)}/>
-        <Statistic text={'positive:'} value={(good/(neutral+bad+good))*100+'%'}/>
-      </div>
-  )
-}
-
-ReactDOM.render(<App/>,document.getElementById('root'))
