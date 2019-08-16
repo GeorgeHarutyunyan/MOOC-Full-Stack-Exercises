af40fec9ad4dce145319a497be9ace4641a3d619
George Harutyunyan <>
2019-08-16 19:32:35 -0400

    part2.1 complete - reupload due to git error

---
+++ src/components/Course.js
@@ -0,0 +1,38 @@
+﻿import React from 'react'
+
+
+const Header = ({ name }) => (
+    <h2>{name}</h2>
+)
+
+const Content = ({ course }) => {
+    const rows = () => course.parts.map(part =>
+        <Part
+            part={part}
+            key={part.id}
+        />)
+
+    return (
+        <ul>
+            {rows()}
+        </ul>
+    )
+}
+
+const Part = ({ part }) => {
+
+    return (
+        <li>
+            <p>{part.name} {part.exercises}</p>
+        </li>
+    )
+}
+
+const Course = ({ course }) => (
+    <div>
+        <Header name={course.name} />
+        <Content course={course} />
+    </div>
+)
+
+export default Course
--- src/index.js
+++ src/index.js
@@ -1,57 +1,10 @@
-import React, { useState } from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Display = ({value}) => (
-        <div>{value}</div>
-)
-
-const Button = ({onClick,text }) => (
-    <button onClick={onClick}>{text}</button>
-)
-
-const App = (props) => {
-    const [selected, setSelected] = useState(0)
-    const [votes,setVotes] = useState(new Array(props.anecdotes.length).fill(0))
-    console.log(votes)
-
-    const handleVotes = () => {
-        const copy = [...votes]
-        copy[selected]+=1
-        setVotes(copy)
-    }
-
-    const handleSelect = () => {
-        setSelected(Math.floor(Math.random() * props.anecdotes.length))
-    }
-
-    return (
-        <div>
-            <h2>Anectode of the day</h2>
-            <Display value={props.anecdotes[selected]}/>
-            <Display value={'has '+votes[selected]+' votes'}/>
-            <p>
-                <Button onClick={() => handleSelect()} text='next anectode' />
-                <Button onClick={() => handleVotes()} text='vote'/>
-            </p>
-            <h2>Anectode with the most votes</h2>
-            <Display value={props.anecdotes[votes.indexOf(Math.max.apply(null,votes))]}/>
-            <Display value={'has '+Math.max.apply(null,votes)+' votes'}/> 
-        </div>
-    )
-}
+import App from './App'
 
 
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App/>,
   document.getElementById('root')
 )
