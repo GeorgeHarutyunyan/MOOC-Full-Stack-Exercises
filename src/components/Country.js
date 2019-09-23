f460ccede19c11e559e0349ef96450ac96d79432
George Harutyunyan <>
2019-09-23 17:21:35 -0400

    part2.14 complete - ran out of free API requests, unable to further test

--- src/components/Country.js
+++ src/components/Country.js
@@ -4,32 +4,32 @@ import axios from 'axios'
 const Country = ({data,singleEntryCheck}) => {
 
     const [expandedView,setExpandedView] = useState(singleEntryCheck)
-    console.log(singleEntryCheck)
-
+    const [weather, setWeather] = useState({})
+ 
     const handleShowChange = () => {
         setExpandedView(!expandedView)
     }
 
     const getWeatherAPI = () => { //self note, do not return the jsx here as axios only returns a promise! As such, I used a secondary function
-        axios.get(`http://api.weatherstack.com/current?access_key=f4279dd8055b9298ce68bd2129651305&query=${data.capital}`)
-        .then(response => {
-            console.log("request is sent")
-            console.log(response)
-            return response.data
-        }
-        )
+        return axios.get(`http://api.weatherstack.com/current?access_key=f4279dd8055b9298ce68bd2129651305&query=${data.capital}`)
+            .then(response => response.data
+            )
+            .then(weatherData => {
+                const weatherDataObject = {
+                    temperature: weatherData.current.temperature,
+                    weather_icon: weatherData.current.weather_icons[0],
+                    wind_speed: weatherData.current.wind_speed,
+                    wind_dir: weatherData.current.wind_dir
+                }
+                setWeather(weatherDataObject)
+            }
+            )
     }
-
-    const parseWeatherData = (data) => (
-        <div>            
-            <h3>Weather in {data.capital}</h3>
-            <h5>temperature: {data.current.temperature}</h5>
-            <img src={data.current.weather_icons[0]} alt={`Temperature in ${data.capital}`}/>
-            <h5>wind: </h5> {data.current.wind_speed} km/h direction {data.current.wind_dir}
-        </div>
-    )
+    
 
     if (expandedView) {
+        getWeatherAPI()
+        console.log('okay a requst..?')
         if (singleEntryCheck) {
             return (
                 <div>
@@ -42,8 +42,13 @@ const Country = ({data,singleEntryCheck}) => {
                             {data.languages.map(language => <li key={language.name}>{language.name}</li>)}
                         </ul>
                     </div>
-                    <img src={data.flag} alt={`Flag of ${data.name}`} width='150'  height='100'/>
-                    {getWeatherAPI().then(data => parseWeatherData(data))}
+                    <img src={data.flag} alt={`Flag of ${data.name}`} width='150' height='100' />
+                    <div>
+                        <h3>Weather in {data.capital}</h3>
+                        <h5>temperature: {weather.temperature}</h5>
+                        <img src={weather.weather_icon} alt={`Temperature in ${data.capital}`}/>
+                        <h5>wind: {weather.wind_speed} km/h direction {weather.wind_dir} </h5> 
+                    </div>
                 </div>
             )
         }
@@ -60,8 +65,13 @@ const Country = ({data,singleEntryCheck}) => {
                             {data.languages.map(language => <li key={language.name}>{language.name}</li>)}
                         </ul>
                     </div>
-                    <img src={data.flag} alt={`Flag of ${data.name}`} width='150'  height='100'/>
-                    {getWeatherAPI().then(data => parseWeatherData(data))}
+                    <img src={data.flag} alt={`Flag of ${data.name}`} width='150' height='100' />
+                    <div>
+                        <h3>Weather in {data.capital}</h3>
+                        <h5>temperature: {weather.temperature}</h5>
+                        <img src={weather.weather_icon} alt={`Temperature in ${data.capital}`}/>
+                        <h5>wind: {weather.wind_speed} km/h direction {weather.wind_dir}</h5>
+                    </div>
                 </div>
             )
         }
