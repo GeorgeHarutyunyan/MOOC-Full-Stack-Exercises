de876f63e67f14e2d3724c7a34c5050f836f88f2
George Harutyunyan <>
2019-10-01 17:01:42 -0400

    part2.19 complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import personService from './services/persons-services'
-import personsServices from './services/persons-services'
+import SuccessNotification from './components/SuccessNotification'
 
 const App = () => {
 	const [persons, setPersons] = useState([]) 
 	const [newName, setNewName ] = useState('')
 	const [newNumber,setNewNumber] = useState('')
-	const [searchValue,setSearchValue] = useState('')
+    const [searchValue, setSearchValue] = useState('')
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [successMessage,setSuccessMessage] = useState(null)
 
 	useEffect (() => {
 		personService
@@ -44,50 +46,67 @@ const App = () => {
 			personService
 			.createPerson(newPerson)
 			.then(response => {
-				setPersons(persons.concat(response))
-				setNewName('')
-				setNewNumber('')
-			})
+                setSuccessMessage(`Successfully added ${newPerson.name} to the phonebook`)
+                setTimeout(() => {
+                    setSuccessMessage(null)
+                },5000)
+                setPersons(persons.concat(response))
+                setNewName('')
+                setNewNumber('')})
+			}
 		}	
-	}
+	
 	
 	const handleDuplicateEntry = () => {
 		if (window.confirm(`${newName} is already in the phonebook. Replace the old number with a new one?`)) {
 			const person = persons.find(p => p.name === newName)
 			const updatedPersonObject = {...person,number:newNumber}
-			personsServices.updatePerson(person.id,updatedPersonObject)
-			setPersons(persons.map(p => p.name === newName ? updatedPersonObject: p))
+            personService.updatePerson(person.id, updatedPersonObject)
+                .then(response => {
+                    setSuccessMessage(`Successfully updated phone number for ${person.name}`)
+                    setTimeout(() => {
+                        setSuccessMessage(null)
+                    }, 5000)
+                    setPersons(persons.map(p => p.name === newName ? updatedPersonObject : p))
+                }
+                )
 		}
 	}
 
     const handleDeletePerson = id => {
-		if (window.confirm(`Delete ${persons.find(p => id === p.id).name} from the phonebook?`)){
-			const request = personService.deletePerson(id)
-			return request.then(response => {
-				setPersons(persons.filter(p => p.id !== id))
-			}
-			)
+        if (window.confirm(`Delete ${persons.find(p => id === p.id).name} from the phonebook?`)) {
+            const person = persons.find(p => p.name === newName)
+			personService.deletePerson(id)
+    			.then(response => {
+                    setSuccessMessage(`Successfully deleted ${person.name}`)
+                    setTimeout(() => {
+                        setSuccessMessage(null)
+                    }, 5000)
+                    setPersons(persons.filter(p => p.id !== id))
+    			}
+    			)
 		}
 
     }
 
 	return (
-    <div>
-    	<h2>Phonebook</h2>
-		<Filter value={searchValue} onChange={handleSearchValue}/>
-      	<form onSubmit={addNewPerson}>
-        	<div>
-				name: <input value={newName} onChange={handleNewName}/>
-				<br/>
-				number: <input value={newNumber} onChange={handleNewNumber}/>
-        	</div>
-        	<div>
-          		<button type="submit">add</button>
-        	</div>
-      	</form>
-      	<h2>Numbers</h2>
+        <div>
+            <h2>Phonebook</h2>
+            <SuccessNotification message={successMessage}/>
+    		<Filter value={searchValue} onChange={handleSearchValue}/>
+          	<form onSubmit={addNewPerson}>
+            	<div>
+    				name: <input value={newName} onChange={handleNewName}/>
+    				<br/>
+    				number: <input value={newNumber} onChange={handleNewNumber}/>
+            	</div>
+            	<div>
+              		<button type="submit">add</button>
+            	</div>
+          	</form>
+          	<h2>Numbers</h2>
             <Persons persons={persons} deletePerson={handleDeletePerson} query={searchValue}/>
-    </div>
+     </div>
   )
 }
 
---
+++ src/components/SuccessNotification.js
@@ -0,0 +1,17 @@
+﻿import React from 'react'
+
+const SuccessNotification = ({ message }) => {
+
+    if (message === null) {
+        return null
+    }
+
+    return (
+        <div className="success">
+            {message}
+        </div>
+           
+    )
+}
+
+export default SuccessNotification
