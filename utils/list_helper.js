1c42ba26c3744145180dabeee2d1ca943c8ebd33
Gevorg Harutyunyan - harutyu6 <george.harutyunyan@mail.utoronto.ca>
2020-02-26 21:08:35 -0500

    part4.5 complete

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,4 +7,16 @@ const totalLikes = blogs => {
     return blogs.reduce((total,amount) => total + amount.likes,0)
 }
 
-module.exports = {dummy,totalLikes}
+const favoriteBlog = blogs => {
+    let topBlog = blogs[0]
+
+    blogs.forEach(blog => {
+        if (blog.likes > topBlog.likes) {
+            topBlog = blog
+        }
+    })
+
+    return topBlog
+}
+
+module.exports = {dummy,totalLikes,favoriteBlog}
